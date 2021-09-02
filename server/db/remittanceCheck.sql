8eafd0799aa125f6689a30c515ef02bdcd51cbcf
seongmin <psm7335@gmail.com>
2021-09-02 23:45:21 +0900

    modify remittanceCheck.sql

--- server/db/remittanceCheck.sql
+++ server/db/remittanceCheck.sql
@@ -2,4 +2,4 @@ create table remittanceCheck(
 id int(11) auto_increment primary key,
 group_id int(11) not null,
 req_user_id int(11) not null,
-res_user_id int(11) not null);
+master_id int(11) not null);
